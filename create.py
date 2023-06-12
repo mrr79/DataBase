7a58b47aa86d617738884abddd51399b4e183168
mrr79 <marianarojas2197@gmail.com>
2023-06-12 03:49:19 -0600

    FINAL CREATE

--- create.py
+++ create.py
@@ -5,3 +5,24 @@ import shutil
 import ast
 
 class Create:
+    def create(self,input_str):
+        # Dividir el input en nombre y atributos
+        partes = input_str.split(" (")
+        nombre = partes[0]
+        atributos = partes[1][:-1].split(",")
+        
+        # Crear la carpeta con el nombre especificado
+        os.makedirs(nombre, exist_ok=True)
+        
+        # Crear el contenido del archivo XML
+        contenido = f"<{nombre}>\n"
+        contenido += f"\t<{nombre}>\n"  # Agregar etiqueta adicional <estudiantes>
+        for atributo in atributos:
+            contenido += f"\t\t<{atributo}></{atributo}>\n"
+        contenido += f"\t</{nombre}>\n"
+        contenido += f"</{nombre}>"  # Cerrar etiqueta <estudiantes> adicional
+        
+        # Crear el archivo XML dentro de la carpeta
+        archivo = open(f"{nombre}/{nombre}.xml", "w")
+        archivo.write(contenido)
+        archivo.close()
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from where import Select
 from insert import Insert
 from delete import Delete
 from  update import Update
+from create import Create
 
 if __name__ == "__main__":
     folder = '/home/mrr/Desktop/DataBase'  # Ruta de la carpeta principal
@@ -11,12 +12,13 @@ if __name__ == "__main__":
     query = Select(folder)
     delete_query = Delete(folder)
     update_query = Update(folder)
+    create_query = Create()
     
 
     # Solicitar al usuario el nombre del archivo XML
 
 
-    opcion = int(input("¿Qué acción deseas realizar? (1: SELECT/JOIN, 2: INSERT, 3: DELETE, 4: UPDATE): "))
+    opcion = int(input("¿Qué acción deseas realizar? (0: CREATE, 1: SELECT/JOIN, 2: INSERT, 3: DELETE, 4: UPDATE): "))
 
     if opcion == 1:
         file_name = input("FROM (sin la extensión .xml): ") + ".xml"
@@ -34,6 +36,11 @@ if __name__ == "__main__":
         file_name = input(" UPDATE ") + ".xml"
         if verification.verify_file(file_name):
             update_query.execute_query(file_name)
+    elif opcion == 0:
+        # Ejemplo de uso
+        file_name = input("CREATE TABLE ")
+        create_query.create(file_name)
+        print("Carpeta y archivo XML creados exitosamente.")
     else:
         print("Opción inválida")
         
