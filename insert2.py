8894c2861fddfef5c8861546015f134f2a8ad14f
mrr79 <marianarojas2197@gmail.com>
2023-06-11 23:56:13 -0600

    join final

--- insert2.py
+++ insert2.py
@@ -1,53 +1,94 @@
 import os
-import shutil
 import xml.etree.ElementTree as ET
+import re
+import shutil
 
-# Ruta al archivo XML original
-ruta_xml_original = '/home/mrr/Desktop/DataBase/productos/productos.xml'
+class Insert:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
 
-# Ruta a la carpeta "Local"
-ruta_local = '/home/mrr/Desktop/DataBase/local'
+    def execute_query(self, input_insert):
+        print("Estoy en insert AAAAAAAAAAAAAAAAA")
 
-# Ruta a la carpeta "productos" dentro de "Local"
-ruta_productos = os.path.join(ruta_local, 'productos')
+        # Define the regular expression pattern to extract the file name and attributes
+        pattern = r'(\w+)\s*\((.+)\)'
 
-# Verificar si la carpeta "productos" existe en la carpeta "Local", si no, crearla
-if not os.path.exists(ruta_productos):
-    os.makedirs(ruta_productos)
+        # Find matches with the pattern in the input_insert string
+        matches = re.match(pattern, input_insert)
 
-# Ruta al archivo XML modificado dentro de la carpeta "productos" en "Local"
-ruta_xml_modificado = os.path.join(ruta_productos, 'productos.xml')
+        if matches:
+            # Extract the file name and attributes
+            file_name = matches.group(1)
+            attributes = matches.group(2).split(',')
+            attributes = [attr.strip() for attr in attributes]
 
-# Copiar el archivo XML original a la carpeta "productos" en "Local"
-shutil.copyfile(ruta_xml_original, ruta_xml_modificado)
+            attributes_str = ','.join(attributes)
+            print(file_name)
+            print(attributes)
+            print("attributes string:", attributes_str)
 
-# Abrir el archivo XML modificado
-tree = ET.parse(ruta_xml_modificado)
-root = tree.getroot()
+            # Construct the file path
+            folder_name = re.sub(r'\W+', '', file_name)
+            xml_file = os.path.join(file_name, file_name + ".xml")
 
-# Crear el nuevo elemento 'productos'
-nuevo_producto = ET.Element('productos')
+            file_tree = ET.parse(xml_file)
+            file_root = file_tree.getroot()
+            file_rows = file_root.findall('.//' + file_name)
 
-# Crear el elemento 'nombre' y establecer su texto
-nombre = ET.SubElement(nuevo_producto, 'nombre')
-nombre.text = 'Nuevo producto en MARIANITAA'
+            print("File insert rows:", file_rows)
 
-# Crear el elemento 'precio' y establecer su texto
-precio = ET.SubElement(nuevo_producto, 'precio')
-precio.text = '420'
+            values_input = input("Ingrese los valores en el formato '(Producto 1, 10.99), (Producto 2, 15.99), ...': ")
+            #values_input = values_input.replace('(', '').replace(')', '')  # Eliminar paréntesis
+            #values_list = [tuple(value.strip().split(',')) for value in values_input.split(',')]
+            #values_list= "["+values_input+"]"
+            values_list = list(values_input)
+            print("SUPUESTA PERRA LISTA DE FKN TUPLAS")
+            print (values_list)
+            #self.insert_xml(file_name, xml_file, attributes_str,values_list)
+            return file_name, attributes_str, xml_file, attributes_str
 
-# Agregar el nuevo producto al elemento raíz
-root.append(nuevo_producto)
+        else:
+            # If no match is found, return None
+            return None
 
-# Guardar los cambios en el archivo XML modificado
-tree.write(ruta_xml_modificado)
+    @staticmethod
+    def insert_xml(file_name, ruta_xml_original, attributes_str,value, ruta_local=None):
+        print("                             estoy en UPDATE DENTRO  ")
+        ruta_local = '/home/mrr/Desktop/DataBase/local'
+        ruta_auxiliar = os.path.join(ruta_local, file_name)
+        if not os.path.exists(ruta_auxiliar):
+            os.makedirs(ruta_auxiliar)
 
-# Preguntar al usuario si desea guardar el archivo modificado en el archivo original
-respuesta = input("¿Deseas guardar el archivo modificado en el archivo original? (Y/N): ")
+        ruta_xml_modificado = os.path.join(ruta_auxiliar, 'productos.xml')
+        shutil.copyfile(ruta_xml_original, ruta_xml_modificado)
 
-if respuesta.upper() == "Y":
-    # Copiar el archivo modificado a la ubicación del archivo original
-    shutil.copyfile(ruta_xml_modificado, ruta_xml_original)
-    print("El archivo modificado se ha guardado tanto en la ubicación original como en la carpeta 'Local'.")
-else:
-    print("El archivo modificado se ha guardado únicamente en la carpeta 'Local'.")
+        tree = ET.parse(ruta_xml_modificado)
+        root = tree.getroot()
+
+        nuevo_ = ET.Element(file_name)
+
+        attributes = attributes_str.split(',')  # Dividir los valores de atributos por coma
+
+        print("SSSSSSSSSSSSSUPUESTA PERRA LISTA DE FKN TUPLAS ABAJJO")
+        print(value)
+
+        for val in value:
+            nuevo_ = ET.Element('nuevo')
+
+            for i, attribute_name in enumerate(attributes):
+                atributo = ET.SubElement(nuevo_, attribute_name)
+                atributo.text = str(val[i])
+
+            root.append(nuevo_)
+
+        # Guardar los cambios en el archivo XML modificado
+        tree.write(ruta_xml_modificado)
+
+        respuesta = input("¿Deseas guardar el archivo modificado en el archivo original? (Y/N): ")
+
+        if respuesta.upper() == "Y":
+            # Copiar el archivo modificado a la ubicación del archivo original
+            shutil.copyfile(ruta_xml_modificado, ruta_xml_original)
+            print("El archivo modificado se ha guardado tanto en la ubicación original como en la carpeta 'Local'.")
+        else:
+            print("El archivo modificado se ha guardado únicamente en la carpeta 'Local'.")
