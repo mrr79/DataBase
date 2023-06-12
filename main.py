75436b1659d73d596afba39c6885e420442fa4e2
mrr79 <marianarojas2197@gmail.com>
2023-06-12 02:54:50 -0600

    UPDATE PT1

--- main.py
+++ main.py
@@ -3,18 +3,20 @@ from verification import Verification
 from where import Select
 from insert import Insert
 from delete import Delete
+from  update import Update
 
 if __name__ == "__main__":
     folder = '/home/mrr/Desktop/DataBase'  # Ruta de la carpeta principal
     verification = Verification(folder)
     query = Select(folder)
     delete_query = Delete(folder)
+    update_query = Update(folder)
     
 
     # Solicitar al usuario el nombre del archivo XML
 
 
-    opcion = int(input("¿Qué acción deseas realizar? (1: SELECT/JOIN, 2: INSERT, 3:DELETE): "))
+    opcion = int(input("¿Qué acción deseas realizar? (1: SELECT/JOIN, 2: INSERT, 3: DELETE, 4: UPDATE): "))
 
     if opcion == 1:
         file_name = input("FROM (sin la extensión .xml): ") + ".xml"
@@ -24,11 +26,14 @@ if __name__ == "__main__":
         file_insert= input("INSERT INTO: ")
         insert_query = Insert(file_insert)
         insert_query.execute_query(file_insert)
-              
     elif opcion == 3:
         file_name = input(" DELETE FROM (sin la extensión .xml): ") + ".xml"
         if verification.verify_file(file_name):
             delete_query.execute_query(file_name)
+    elif opcion == 4:
+        file_name = input(" DELETE FROM (sin la extensión .xml): ") + ".xml"
+        if verification.verify_file(file_name):
+            update_query.execute_query(file_name)
     else:
         print("Opción inválida")
         
